80135e3fad3cb4e2d0ac37eed53e4b1d2f0e459e
Sam Gentiile <managedcode44@hotmail.com>
2018-12-01 23:11:35 -0500

    Performed EF dattabase migration with Identity database

--- Identity.AuthServer/Startup.cs
+++ Identity.AuthServer/Startup.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Identity.AuthServer.Infrastructure;
+using Identity.AuthServer.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +28,15 @@ namespace Identity.AuthServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["ConnectionString"];
+
+            services.AddDbContext<BFIdentityContext>(options =>
+            {
+                options.UseSqlServer(connection);
+            });
+
+            services.AddIdentity<UserEntity, UserRoleEntity>().AddEntityFrameworkStores<BFIdentityContext>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
